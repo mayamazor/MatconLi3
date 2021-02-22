1ad2a2dc63eeb5e5ffdd01fdcfe0ada26fa147c5
Maya Mazor <meyzor101@cs.colman.ac.il>
2021-02-22 11:55:07 +0200

    all of us beginning1

--- app/src/main/java/com/example/matconli3/HomeFragment.java
+++ app/src/main/java/com/example/matconli3/HomeFragment.java
@@ -3,10 +3,12 @@ package com.example.matconli3;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.matconli3.model.Category;
 import com.example.matconli3.model.Model;
@@ -14,7 +16,7 @@ import com.example.matconli3.model.Model;
 import java.util.List;
 
 
-public class CategoryListFragment extends Fragment {
+public class HomeFragment extends Fragment {
 
 
 
@@ -22,8 +24,15 @@ public class CategoryListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_category_list, container, false);
-        List<Category> data= Model.instance.getAllCategories();
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+       Button regbtn=view.findViewById(R.id.btn_register);
+       regbtn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Navigation.findNavController(v).navigate(R.id.action_home_to_register);
+
+           }
+       });
         return view;
     }
 }
